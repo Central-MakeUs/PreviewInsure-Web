6d4cd6cda84d95ba37c46b670e08ea16f064e93f
seehun <hwangbird99@gmail.com>
2024-07-12 17:44:21 +0900

    feat : icon 추가
    - icon을 svg형태로 저장(파일이름은 figma대로)
    - svg를 component형태로 사용할 수 있도록 설

--- src/screens/testScreen2.tsx
+++ src/screens/testScreen2.tsx
@@ -1,9 +1,16 @@
+import { ReactComponent as Chatting } from '@/assets/icons/Chatting.svg';
+import { ReactComponent as X } from '@/assets/icons/X.svg';
+
 const testPage2 = () => {
   return (
     <div>
-      <h1>test2</h1>
-      <p>2번 페이지</p>
-      <p>티즈 테스트용</p>
+      <div>
+        <h1>test2</h1>
+        <p>2번 페이지</p>
+        <p>티즈 테스트용</p>
+      </div>
+      <Chatting width={30} height={30} fill="orange" />
+      <X width={30} height={30} fill="orange" />
     </div>
   );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import svgr from '@svgr/rollup';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    //svg
+    svgr(),
+  ],
   // 절대경로 추가
   resolve: {
     alias: {
