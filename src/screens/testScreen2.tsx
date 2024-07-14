36497d09a28fcebfcbcc9568678e71bcba720378
seehun <hwangbird99@gmail.com>
2024-07-14 19:06:53 +0900

    feat: 공통 component 작업
    - SearchBar
    - Selector
    - Loading ( 라이브러리 사용)
    - Process
    - 각 component마다 props type으로 사용법 기재

--- src/screens/testScreen2.tsx
+++ src/screens/testScreen2.tsx
@@ -1,7 +1,21 @@
+import React, { useState } from 'react';
 import { ReactComponent as Chatting } from '@/assets/icons/Chatting.svg';
-import { ReactComponent as X } from '@/assets/icons/X.svg';
+import { ReactComponent as Magnifier } from '@/assets/icons/Magnifier.svg';
+import SearchBar from '@components/commons/SearchBar';
+import Selector from '@components/commons/Selector';
+import Loading from '@components/commons/Loading';
+import Process from '@components/commons/Process';
 
 const testPage2 = () => {
+  //searchBar icon 클릭 시
+  const SearchBarHandler = () => {
+    console.log(1);
+  };
+  //selector check 여부
+  const [check, setCheck] = useState(false);
+
+  // process active step
+  const [activeStep, setActiveStep] = useState(1);
   return (
     <div>
       <div>
@@ -9,8 +23,28 @@ const testPage2 = () => {
         <p>2번 페이지</p>
         <p>티즈 테스트용</p>
       </div>
+      <br />
       <Chatting width={30} height={30} fill="orange" />
-      <X width={30} height={30} fill="orange" />
+      {/* SearchBar */}
+      <SearchBar
+        backgroundColor={'#fff'}
+        icon={<Magnifier width={27} height={29} fill={'#000'} />}
+        handler={SearchBarHandler}
+      />
+      {/* Selector */}
+      <Selector check={check} setCheck={setCheck} />
+      {/* Loading */}
+      <Loading type={'spinningBubbles'} color={'#6879FB'} width={69.33} height={69.33} />
+      {/* process  밑에는 샘플 버튼  */}
+      <Process stepNumber={4} activeStep={activeStep} />
+      <br />
+      <br />
+      <button disabled={activeStep === 1} onClick={() => setActiveStep(activeStep - 1)}>
+        prev
+      </button>
+      <button disabled={activeStep === 4} onClick={() => setActiveStep(activeStep + 1)}>
+        next
+      </button>
     </div>
   );
 };
