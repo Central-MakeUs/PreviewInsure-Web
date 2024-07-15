4b2e11526e1541a1530137d6640f1dd6008f32df
yeongeun <duddms0190@naver.com>
2024-07-15 22:13:42 +0900

    ✨ Feat: Pretendard 폰트 설정

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,48 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const styled = { createGlobalStyle };
+
+const GlobalStyle = styled.createGlobalStyle`
+  // 브라우저 환경마다 다른 css를 초기화 한다.
+  ${reset}
+
+  html {
+    font-size: 62.5%; //10px
+    height: 100%;
+  }
+
+  body,
+  #root {
+    height: 100%;
+  }
+
+  *,
+  *::before,
+  *::after {
+    /* 테두리를 기준으로 크기를 정함. */
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Pretendard', sans-serif;
+    min-width: 320px;
+    margin: 0;
+    min-height: 100vh;
+  }
+
+  a {
+    color: #1e87f0;
+    text-decoration: inherit;
+  }
+  a:hover {
+    color: #0f6ecd;
+  }
+
+  b,
+  strong {
+    font-weight: bolder;
+  }
+`;
+
+export default GlobalStyle;
