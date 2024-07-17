30e32f5a77c617cec10df7aceb464843824dc285
yeongeun <duddms0190@naver.com>
2024-07-18 03:01:42 +0900

    ♻️ Refactor: styled component prop wranning 제거

--- src/components/commons/Cartegory.tsx
+++ src/components/commons/Cartegory.tsx
@@ -10,16 +10,16 @@ const Cartegory = ({ text, color, handler }: CategoryProps) => {
   const bgColor = color === 'Primary' ? theme?.colors.Primary100 : theme?.colors.Black100;
 
   return (
-    <Container tintColor={tintColor} bgColor={bgColor}>
+    <Container tintcolor={tintColor} bgcolor={bgColor}>
       <p>{text}</p>
       <X width={21} height={21} fill={tintColor} onClick={handler} />
     </Container>
   );
 };
 
-const Container = styled.div<{ tintColor?: string; bgColor?: string }>`
+const Container = styled.div<{ tintcolor?: string; bgcolor?: string }>`
   border-radius: 1.3rem;
-  background-color: ${(prop) => prop.bgColor};
+  background-color: ${(prop) => prop.bgcolor};
   padding: 1rem 2.4rem 1rem 3.1rem;
 
   display: flex;
@@ -30,13 +30,13 @@ const Container = styled.div<{ tintColor?: string; bgColor?: string }>`
   p {
     font-size: 3rem;
     font-weight: 600;
-    color: ${(prop) => prop.tintColor};
+    color: ${(prop) => prop.tintcolor};
     padding-right: 1.6rem;
   }
 
   path {
     stroke-width: 2;
-    stroke: ${(prop) => prop.tintColor};
+    stroke: ${(prop) => prop.tintcolor};
   }
 `;
 
