db41f7ae4fcb6e0279c23e694ec498bca3374785
yeongeun <duddms0190@naver.com>
2024-07-22 03:43:44 +0900

    🐛 Fix: Google Login redirect uri 수정

--- src/utils/LoginAndRegister/Login.ts
+++ src/utils/LoginAndRegister/Login.ts
@@ -1,5 +1,7 @@
 const GOOGLE_CLIENT_ID = import.meta.env.VITE_APP_GOOGLE_CLIENT_ID;
-const GOOGLE_REDIRECT_URI = 'http://localhost:5173/callback/google';
+// const GOOGLE_REDIRECT_URI = import.meta.env.VITE_APP_GOOGLE_REDIRECnpT_URI;
+
+const GOOGLE_REDIRECT_URI = window.location.protocol + '//' + window.location.host + '/callback/google';
 
 export const googleLogin = () => {
   window.location.href = `https://accounts.google.com/o/oauth2/auth?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${GOOGLE_REDIRECT_URI}&response_type=code&scope=openid email profile`;
