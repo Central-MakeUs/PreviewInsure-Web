434e7ed3780e2ead71335f33b8e3b12754ef5988
seehun <hwangbird99@gmail.com>
2024-07-26 17:58:13 +0900

    Feat: InsuePlanner 스크린
    - 기본화면, 로딩화면 컴포넌트화
    - loading에 따라 다른 화면 렌더링
    - type 정의

---
+++ src/components/InsuePlanner/InsuePlannerLoading.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ReactComponent as SearchingForContent } from '@/assets/icons/InsuePlanner/SearchingForContent.svg';
+import type { InsuePlannerLoadingProps } from '@/types/InsuePlannerComponents';
+
+function InsuePlannerLoading({ SubtitleSize }: InsuePlannerLoadingProps) {
+  return (
+    <Container>
+      <SearchingForContent />
+      <Title SubtitleSize={SubtitleSize}>
+        <TitleP>추천해주는 정보는 인공지능을 통해 분석된 내용으로, 정확하지 않을 수 있어요.</TitleP>
+        <TitleP>보험 가입시 제공되는 상품 설명서 및 이용 약관에서 더 정확한 정보를 얻을 수 있어요.</TitleP>
+      </Title>
+    </Container>
+  );
+}
+
+export default InsuePlannerLoading;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Title = styled.p<{ SubtitleSize: string }>`
+  font-size: ${({ SubtitleSize }) => SubtitleSize};
+  font-weight: 400;
+  color: ${({ theme }) => theme.colors.Black500};
+  line-height: 1.4;
+  text-align: center;
+`;
+
+const TitleP = styled.p``;
