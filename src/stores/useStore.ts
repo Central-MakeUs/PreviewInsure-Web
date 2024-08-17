5b85bc8c1fa070c29dde53bb50b75c561e70068a
seehun <hwangbird99@gmail.com>
2024-08-17 13:57:01 +0900

    feat : zustand Persist 적용
    zustand Persist 적용

--- src/stores/useStore.ts
+++ src/stores/useStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
 
 export interface State {
   accessToken: string;
@@ -12,39 +13,63 @@ export interface State {
   setTempToken: (token: string) => void;
 }
 
-const getInitialState = (): State => {
-  const storedAccessToken = localStorage.getItem('accessToken');
-  const storedNickName = localStorage.getItem('nickName');
-  const isLogin = !!storedAccessToken;
+// const getInitialState = (): State => {
+//   const storedAccessToken = localStorage.getItem('accessToken');
+//   const storedNickName = localStorage.getItem('nickName');
+//   const isLogin = !!storedAccessToken;
 
-  return {
-    accessToken: storedAccessToken || '',
-    nickName: storedNickName || '',
-    isLogin,
-    login: () => {},
-    logOut: () => {},
+//   return {
+//     accessToken: storedAccessToken || '',
+//     nickName: storedNickName || '',
+//     isLogin,
+//     login: () => {},
+//     logOut: () => {},
 
-    temporaryToken: '',
-    setTempToken: () => {},
-  };
-};
+//     temporaryToken: '',
+//     setTempToken: () => {},
+//   };
+// };
 
-export const useStore = create<State>((set, get) => ({
-  ...getInitialState(),
-  login: (token, nickName) => {
-    localStorage.setItem('accessToken', token);
-    localStorage.setItem('nickName', nickName);
-    set({ accessToken: token, nickName, isLogin: true, temporaryToken: '' });
-  },
-  logOut: () => {
-    localStorage.removeItem('accessToken');
-    localStorage.removeItem('nickName');
-    set({ accessToken: '', nickName: '', isLogin: false, temporaryToken: '' });
-  },
+// export const useStore = create<State>((set, get) => ({
+//   ...getInitialState(),
+//   login: (token, nickName) => {
+//     localStorage.setItem('accessToken', token);
+//     localStorage.setItem('nickName', nickName);
+//     set({ accessToken: token, nickName, isLogin: true, temporaryToken: '' });
+//   },
+//   logOut: () => {
+//     localStorage.removeItem('accessToken');
+//     localStorage.removeItem('nickName');
+//     set({ accessToken: '', nickName: '', isLogin: false, temporaryToken: '' });
+//   },
 
-  setTempToken: (token) => {
-    set({
-      temporaryToken: token,
-    });
-  },
-}));
+//   setTempToken: (token) => {
+//     set({
+//       temporaryToken: token,
+//     });
+//   },
+// }));
+
+export const useStore = create<State>()(
+  persist(
+    (set) => ({
+      accessToken: '',
+      nickName: '',
+      isLogin: false,
+      temporaryToken: '',
+      login: (token, nickName) => {
+        set({ accessToken: token, nickName, isLogin: true, temporaryToken: '' });
+      },
+      logOut: () => {
+        set({ accessToken: '', nickName: '', isLogin: false, temporaryToken: '' });
+      },
+      setTempToken: (token) => {
+        set({ temporaryToken: token });
+      },
+    }),
+    {
+      name: 'auth-storage',
+      storage: createJSONStorage(() => localStorage),
+    },
+  ),
+);
