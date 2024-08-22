86131606f36a9b30ea0a0332e84d3ff58dd15e7e
seehun <hwangbird99@gmail.com>
2024-08-22 19:51:29 +0900

    🐛 Fix: 인슈플래너 웹 히스토리 복구
    인슈플래너 답변페이지 모바일 기준사이즈 변경

--- src/components/InsuePlanner/InsuePlannerAnswer.tsx
+++ src/components/InsuePlanner/InsuePlannerAnswer.tsx
@@ -80,9 +80,10 @@ function InsuePlannerAnswer({
   // }, [questionDetailQuery.isFetched]);
 
   //mobile design
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 99999); //767
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 767); //767
   window.addEventListener('resize', function (e: any) {
-    if (e.currentTarget.innerWidth <= 99999) {
+    if (e.currentTarget.innerWidth <= 767) {
+      // 다시 수정
       //767  -> 기존 모바일
       setIsMobile(true);
     } else {
