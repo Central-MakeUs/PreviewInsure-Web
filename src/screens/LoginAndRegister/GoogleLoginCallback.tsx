e325e20206d21ce83327cd14ed18bd567e5663ca
yeongeun <duddms0190@naver.com>
2024-08-30 17:14:44 +0900

    🐛 Fix: 구글 로그인 에러 메세지

--- src/screens/LoginAndRegister/GoogleLoginCallback.tsx
+++ src/screens/LoginAndRegister/GoogleLoginCallback.tsx
@@ -1,33 +1,37 @@
+import Button from '@components/commons/Button';
 import Loading from '@components/commons/Loading';
 import { useStore } from '@stores/useStore';
+import media from '@styles/media';
 import axiosInstance from '@utils/axios';
 import { closeNewTab } from '@utils/common/openNewTab';
 import { platform } from 'os';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 
 const GoogleLoginCallback = () => {
   const navigate = useNavigate();
   const { login, setTempToken } = useStore();
   const [accessToken, setAccessToken] = useState<string>('');
   const [accessNickname, setAccessNickname] = useState<string>('');
-
-  // 이미 가입한 유저일 시 : 메인 페이지로 이동
-  const handleHome = () => {
-    navigate('/');
-    window.location.reload();
-  };
-
-  // 처음 가입한 유저일 시 : 닉네임 설정 페이지로 이동
-  const handleRegist = () => {
-    navigate('/registerAgree');
-    window.location.reload();
-  };
+  const [errorMessage, setErrorMessage] = useState<string | undefined>(undefined);
 
   // 현재 url에서 code 부분 추출
   const params = new URLSearchParams(window.location.search);
   const code = params.get('code');
 
+  // 1
+  useEffect(() => {
+    if (code) {
+      handleLoginPost(code);
+    } else {
+      console.log('로그인 재시도하세요.');
+      closeNewTab(); //WebView로 전송
+      setErrorMessage('로그인에 실패하였습니다.');
+    }
+  }, [code, navigate]);
+
+  // 2
   const handleLoginPost = async (code: string) => {
     //코드 받아와지는 거 확인됐음.
     // console.log(code);
@@ -43,20 +47,14 @@ const GoogleLoginCallback = () => {
       console.log('google login error', error);
       if (error?.response.data.code === -1004) {
         alert('탈퇴한 유저는 재가입할 수 없습니다.');
-        navigate('/login');
+        setErrorMessage('탈퇴한 유저는 재가입할 수 없습니다.');
+      } else {
+        setErrorMessage('로그인에 실패하였습니다.');
       }
     }
   };
 
-  useEffect(() => {
-    if (code) {
-      handleLoginPost(code);
-    } else {
-      console.log('로그인 재시도하세요.');
-      closeNewTab(); //WebView로 전송
-    }
-  }, [code, navigate]);
-
+  // 3
   useEffect(() => {
     if (accessToken) {
       console.log('accessToken', accessToken);
@@ -94,11 +92,39 @@ const GoogleLoginCallback = () => {
     >
       <Loading type={'spinningBubbles'} color={'#6879FB'} width={69.33} height={69.33} />
       <br />
+      {errorMessage && <Message>{errorMessage}</Message>}
+      {errorMessage && (
+        <Button
+          size="big"
+          kind="outline"
+          handler={() => {
+            navigate('/login');
+          }}
+        >
+          로그인 화면으로
+        </Button>
+      )}
       {/* {code ? <h1>로그인 중입니다.</h1> : <h1>로그인에 실패하였습니다.</h1>} */}
 
       {/* <button onClick={handleCriticalLogin}>일단 로그인하기</button> */}
     </div>
   );
 };
+
+const Message = styled.p`
+  width: fit-content;
+  margin: 14rem auto;
+  color: ${({ theme }) => theme.colors.Black300};
+  font-size: ${({ theme }) => theme.fontSizes.paragraph};
+  font-weight: 500;
+  white-space: pre-wrap;
+  text-align: center;
+  line-height: normal;
+
+  ${media.mobile`
+    margin: 4rem auto;
+    font-size: 14px;
+  `}
+`;
 
 export default GoogleLoginCallback;
